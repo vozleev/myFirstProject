cf33a7e5853b7e40bce71915a3906d6bfb2551a3
Vozleev Yury <yvozleev@ourgapps.com>
2024-11-18 21:22:18 +0300

    Initial commit

---
+++ sdfgsfaga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sdfgsfaga
+//
+//  Created by Yury Vozleev on 18.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
